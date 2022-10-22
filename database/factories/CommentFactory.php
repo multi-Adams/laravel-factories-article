19b834207c7563d209395069ea32e5b0b177246d
Adebayo Adams <adamsmuyiwa1987@gmail.com>
2022-10-22 13:38:47 +0100

    initial commit

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Comment;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    protected $model = Comment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
+            'body' => $this->faker->sentence(45),
+            'approved' => $this->faker->boolean(),
+            'likes' => $this->faker->randomNumber(5)
+        ];
+    }
+}
